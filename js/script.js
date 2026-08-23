2b5935826e9b5da16d80eaf6f0482a73702341cc
Alberto Pedraza <washburnuser@gmail.com>
2026-08-23 10:00:57 -0600

    Se agregó la navbar con logo, el footer y las variables en el script de js

---
+++ js/script.js
@@ -0,0 +1,24 @@
+/**
+ * Se agregarán los nombres de los álbumes y su año de publicación así como si son de la década de los 2020's
+ */
+
+let album1 = "PAPOTA"
+console.log(album1)
+let añoAlbum1 = 2025
+console.log(añoAlbum1)
+let isOne2020 = true
+console.log("De la década de los 2020's: "+isOne2020)
+
+let album2 = "Ghost City"
+console.log(album2)
+let añoAlbum2 = 2018
+console.log(añoAlbum2)
+let isTwo2020 = false
+console.log("De la década de los 2020's: "+isTwo2020)
+
+let album3 = "DeBÍ TiRAR MáS FOToS"
+console.log(album3)
+let añoAlbum3 = 2025
+console.log(añoAlbum3)
+let isThree2020 = true
+console.log("De la década de los 2020's: "+isThree2020)
